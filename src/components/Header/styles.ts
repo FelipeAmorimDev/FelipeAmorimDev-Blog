72cde979f6198fcb530caa86dc524f594c125885
Felipe Amorim <felipeamorim.go@gmail.com>
2023-12-07 18:26:36 -0300

    Ajustado caminho do headerbg

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,8 +1,9 @@
 import styled from 'styled-components'
+import bgheader from '../../../public/bg-header.png'
 
 export const HeaderContainer = styled.header`
   padding-top: 64px;
-  background: url('../../../public/bg-header.png');
+  background: url(${bgheader});
   background-size: 100%;
   background-position: center;
 
